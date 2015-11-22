50dd5034321645fbe4e399c99e11155e6235b782
Harrison <harrison181997@gmail.com>
2015-11-22 15:08:06 +0000

    Small changes
    I just changed the theme

--- Health Watch/obj/Debug/android/src/health_watch/health_watch/R.java
+++ Health Watch/obj/Debug/android/src/health_watch/health_watch/R.java
@@ -20,8 +20,9 @@ public final class R {
         public static final int News=0x7f060003;
         public static final int Rate=0x7f060002;
         public static final int Search=0x7f060001;
+        public static final int editText1=0x7f060005;
+        public static final int editText3=0x7f060006;
         public static final int imageView1=0x7f060000;
-        public static final int textView1=0x7f060005;
     }
     public static final class layout {
         public static final int main=0x7f030000;
